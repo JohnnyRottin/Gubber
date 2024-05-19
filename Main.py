2680e2284f118aa447a536603916e0fa65681e48
Jonathan <jonathansirrine@gmail.com>
2024-05-19 14:14:39 -0600

    pseudocode

--- Main.py
+++ Main.py
@@ -15,5 +15,19 @@ def Guess_a_number():
     else :
         print("Okay quitter!")
 
+#define the main function here like...
+# def main():
+    # Take in user input
+    # switch statement with cases
+        # case for the guess a number
+            # call the gues a number function
+        # case for quit
+            # call them a quitter and don't call any other function
+        # default case
+            # say "Invalid option" or something, and call the 'main' function again
+
+# We still call the greeting function...
 Greeting()
-Guess_a_number()
+
+# but now we also call the main function here like...
+# main()
